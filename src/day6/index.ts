3eeeb3faa872d29d97d39803b79fb52f665fe09a
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-12-07 18:26:32 +0100

    day 6 part 2

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,7 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { parseLines } from '../utils';
-import { group } from 'node:console';
 
 function readInput(): string {
   const dayFolder = path.basename(__dirname);
@@ -43,58 +42,47 @@ export function solve(): { part1: number; part2: number } {
   }, 0);
 
   // Part 2 - Doesn't work, will fix later
-  const rowsUntouched = parseLines(input);
-  console.log({ rowsUntouched });
-  const firstRow = rowsUntouched[0];
-  const longestRowLength = Math.max(...rowsUntouched.map((row) => row.length));
+  const rowsPart2 = parseLines(input).map((line) => line.split(''));
   const isNumber = (str: string) => /[0-9]+/g.test(str);
   const isOperator = (str: string) => str === '+' || str === '*';
-  let sum = 0;
-  console.log({ firstRow });
 
   const groups: RetVal<string>[] = [];
+  rowsPart2[0].reduceRight(
+    (group, _, columnIndex) => {
+      const allColumnEntries = rowsPart2.reduce((acc, row) => {
+        acc.push(row[columnIndex]);
+        return acc;
+      }, []);
 
-  for (
-    let columnIndex = longestRowLength - 1;
-    columnIndex >= 0;
-    columnIndex--
-  ) {
-    let everyRowItemWasEmpty = true;
-    let numbersInCol = '';
-    let operator = '';
-    for (let rowIndex = 0; rowIndex < rowsUntouched.length; rowIndex++) {
-      const currentValue = rowsUntouched[rowIndex]?.[columnIndex];
+      const numsCombined = allColumnEntries
+        .filter((entry) => isNumber(entry))
+        .join('');
+      const operator = allColumnEntries.find((entry) => isOperator(entry));
 
-      // console.log({
-      //   currentValue,
-      //   isNum: isNumber(currentValue),
-      // });
+      if (numsCombined) group.nums.push(numsCombined);
+      if (operator) group.operator = operator;
 
-      if (isNumber(currentValue)) {
-        numbersInCol += currentValue;
-        everyRowItemWasEmpty = false;
+      if (
+        allColumnEntries.every((entry) => entry === ' ') ||
+        columnIndex === 0
+      ) {
+        groups.push(group);
+        group = {
+          nums: [],
+          operator: '',
+        };
       }
-      if (isOperator(currentValue)) {
-        operator = currentValue;
-        everyRowItemWasEmpty = false;
-      }
 
-      // console.log({
-      //   numbersInCol,
-      //   isLast: rowIndex === rowsUntouched.length - 1,
-      //   everyRowItemWasEmpty,
-      // });
-    }
+      return group;
+    },
+    { nums: [], operator: '' } as RetVal<string>
+  );
 
-    if (numbersInCol && operator) {
-      groups.push({
-        operator: operator,
-        nums: [numbersInCol],
-      });
-    }
-  }
+  const part2 = groups.reduce(
+    (acc, { nums, operator }) =>
+      (acc += calculate[operator](...nums.map(Number))),
+    0
+  );
 
-  console.log(groups);
-
-  return { part1, part2: 0 };
+  return { part1, part2 };
 }
