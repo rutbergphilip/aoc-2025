460647531f070f201f94dcde390ef3b3e4ee4b61
Philip Rutberg <54883264+rutbergphilip@users.noreply.github.com>
2025-12-07 16:02:51 +0100

    Implement part of the solution for day 6, including initial parsing and grouping logic; commented out incomplete part 2 logic for future work.

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { parseLines } from '../utils';
+import { group } from 'node:console';
 
 function readInput(): string {
   const dayFolder = path.basename(__dirname);
@@ -41,41 +42,59 @@ export function solve(): { part1: number; part2: number } {
     return (sum += total);
   }, 0);
 
-  // const part2 = rows[0].reduce((sum, _, index) => {
-  //   const { nums, operator } = rows.reduce(
-  //     (nums, row) => {
-  //       if (!/[0-9]+/g.test(row[index])) {
-  //         Object.assign(nums, { operator: row[index] });
-  //         return nums;
-  //       }
+  // Part 2 - Doesn't work, will fix later
+  const rowsUntouched = parseLines(input);
+  console.log({ rowsUntouched });
+  const firstRow = rowsUntouched[0];
+  const longestRowLength = Math.max(...rowsUntouched.map((row) => row.length));
+  const isNumber = (str: string) => /[0-9]+/g.test(str);
+  const isOperator = (str: string) => str === '+' || str === '*';
+  let sum = 0;
+  console.log({ firstRow });
 
-  //       nums.nums.push(row[index]);
-  //       return nums;
-  //     },
-  //     { nums: [], operator: '' } as RetVal<string>
-  //   );
+  const groups: RetVal<string>[] = [];
 
-  //   console.log({ nums });
+  for (
+    let columnIndex = longestRowLength - 1;
+    columnIndex >= 0;
+    columnIndex--
+  ) {
+    let everyRowItemWasEmpty = true;
+    let numbersInCol = '';
+    let operator = '';
+    for (let rowIndex = 0; rowIndex < rowsUntouched.length; rowIndex++) {
+      const currentValue = rowsUntouched[rowIndex]?.[columnIndex];
 
-  //   const startingPosition = Math.max(...nums.map((num) => num.length)) - 1;
-  //   const newNums: number[] = [];
+      // console.log({
+      //   currentValue,
+      //   isNum: isNumber(currentValue),
+      // });
 
-  //   for (let i = startingPosition; i >= 0; i--) {
-  //     let total = '';
+      if (isNumber(currentValue)) {
+        numbersInCol += currentValue;
+        everyRowItemWasEmpty = false;
+      }
+      if (isOperator(currentValue)) {
+        operator = currentValue;
+        everyRowItemWasEmpty = false;
+      }
 
-  //     for (let j = 0; j < nums.length; j++) {
-  //       const numToAdd = nums[j][i];
-  //       if (!numToAdd) continue;
-  //       total = total.concat(numToAdd);
-  //     }
+      // console.log({
+      //   numbersInCol,
+      //   isLast: rowIndex === rowsUntouched.length - 1,
+      //   everyRowItemWasEmpty,
+      // });
+    }
 
-  //     newNums.push(Number(total));
-  //   }
+    if (numbersInCol && operator) {
+      groups.push({
+        operator: operator,
+        nums: [numbersInCol],
+      });
+    }
+  }
 
-  //   console.log({ newNums });
-
-  //   return (sum += calculate[operator](...newNums));
-  // }, 0);
+  console.log(groups);
 
   return { part1, part2: 0 };
 }
